15e6fb2dd09aeb28d44ad881c22eec1de815d769
Nipun Tyagi <tyaginipun2004@gmail.com>
2025-12-18 21:43:30 +0530

    final clean build

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: "/codewithtechno-website/",
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
+
+
 export default defineConfig({
   base: "/codewithtechno-website/",
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
